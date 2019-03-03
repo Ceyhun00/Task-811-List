fc526fe45c9383690369ac75e04be4f1cbad3ca9
Ceyhun00 <43810103+Ceyhun00@users.noreply.github.com>
2019-03-03 18:13:46 +0300

    Update Program.cs

--- CircleList/Program.cs
+++ CircleList/Program.cs
@@ -6,26 +6,19 @@ namespace CircleList
     {
         static void Main(string[] args)
         {
-            //-----Создание списка из файла-----
             var list = new CircleList("In.txt");
 
-            //-----Вывод содержимого файла на экзан-----
             list.Show();
 
-            //-----Вставка нового участника в список-----
             var p = new Parcipiant { Name = "qwe", Gender = "male" };
             list.Insert(p);
 
-            //-----Удаление участника из списка по имени-----
             list.Delete("qwe");
 
-            //-----Сортировка списка начиная с выбранного имени-----
             list.Sort("Adam");
 
-            //-----Удаление каждого k-ого и возврат последнего оставшегося-----
             list.Last(3);
 
-            //-----Построение двух списков, 1 из мужчин, другой из женщин-----
             var genders = list.Gender();
 
             Console.ReadKey();
